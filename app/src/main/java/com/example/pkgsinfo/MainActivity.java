a7a9a0cd019622c3f6ef313c361e3c2597e1ab19
v01z <v01z@mail.ru>
2023-06-14 00:42:15 +0300

    dev_pin starts to differ from parent

--- app/src/main/java/com/example/pkgsinfo/MainActivity.java
+++ app/src/main/java/com/example/pkgsinfo/MainActivity.java
@@ -7,10 +7,17 @@ import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.pkgsinfo.databinding.ActivityMainBinding;
 
+import java.io.IOException;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.Socket;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
@@ -24,6 +31,46 @@ public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
 
+    TextView tv, editText;
+    private void addInfo(){
+       final PackageManager pm = getPackageManager();
+       List<ApplicationInfo> packages = pm.getInstalledApplications(PackageManager.GET_META_DATA);
+
+       tv.setText("");
+
+       tv.append("\nxxxxxxxxxxxxxxxxxxxxxx\n");
+       tv.append("\nCount of installed pkgs: " + packages.size());
+       tv.append("\nxxxxxxxxxxxxxxxxxxxxxx\n");
+
+       String querriedName = editText.getText().toString();
+
+
+       if(querriedName.equals("*"))
+       {
+
+           for(ApplicationInfo packageInfo : packages) {
+               tv.append("\n\nInstalled package :" + packageInfo.packageName);
+               tv.append("\nSource dir : " + packageInfo.sourceDir);
+               tv.append("\nClass name: " + packageInfo.className);
+               tv.append("\nLaunch Activity :" + pm.getLaunchIntentForPackage(packageInfo.packageName));
+               tv.append("\n***********************\n");
+           }
+       }
+       else {
+
+           for (ApplicationInfo packageInfo : packages) {
+
+               String currentPackageName = packageInfo.packageName;
+               if (currentPackageName.contains(querriedName)) {
+                   tv.append("\n\nInstalled package :" + currentPackageName);
+                   tv.append("\nSource dir : " + packageInfo.sourceDir);
+                   tv.append("\nClass name: " + packageInfo.className);
+                   tv.append("\nLaunch Activity :" + pm.getLaunchIntentForPackage(packageInfo.packageName));
+                   tv.append("\n***********************\n");
+               }
+           }
+       }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,33 +80,47 @@ public class MainActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         // Example of a call to a native method
-        TextView tv = binding.sampleText;
+        tv = binding.sampleText;
         tv.setMovementMethod(new ScrollingMovementMethod());
-        tv.setText(getFileTextBuffer("/proc/cpuinfo"));
+        //tv.setText(getFileTextBuffer("/proc/cpuinfo"));
+        tv.setText("youtube");
 
-        final PackageManager pm = getPackageManager();
-//get a list of installed apps.
-        List<ApplicationInfo> packages = pm.getInstalledApplications(PackageManager.GET_META_DATA);
 
-        for (ApplicationInfo packageInfo : packages) {
-            Log.d(TAG, "Installed package :" + packageInfo.packageName);
-            Log.d(TAG, "Source dir : " + packageInfo.sourceDir);
-            Log.d(TAG, "Launch Activity :" + pm.getLaunchIntentForPackage(packageInfo.packageName));
+        editText = findViewById(R.id.edit_query);
+        Button updateButton = findViewById(R.id.update_button);
+        updateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               addInfo();
+            }
+        });
+
+        addInfo();
+        /*
+        Socket socket = new Socket();
+        try {
+            socket.connect(new InetSocketAddress("google.com", 80));
+        }catch(IOException e)
+        {
+            Log.d(TAG, "socket.connect()");
+            //
         }
-        //
-        tv.append("\nxxxxxxxxxxxxxxxxxxxxxx\n");
-        tv.append("\nCount of installed pkgs: " + packages.size());
-        tv.append("\nxxxxxxxxxxxxxxxxxxxxxx\n");
-        //
-        for (ApplicationInfo packageInfo : packages) {
-            tv.append("\n\nInstalled package :" + packageInfo.packageName);
-            tv.append("\nSource dir : " + packageInfo.sourceDir);
-            tv.append("\nLaunch Activity :" + pm.getLaunchIntentForPackage(packageInfo.packageName));
-            tv.append("\n***********************\n");
+
+        //System.out.println(socket.getLocalAddress());
+        tv.append("\nIP: " + socket.getLocalAddress());
+
+        try {
+
+        socket.close();
+        }catch(IOException e)
+        {
+            Log.d(TAG, "socket.close()");
         }
+        */
+        //
+
         tv.append("\n\n********* Have a nice day! *****************\n\n");
 
-// the getLaunchIntentForPackage returns an intent that you can use with startActivity()
 
     }
 
